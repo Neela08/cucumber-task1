e3ee932ce58a3dc5328a9d2eac0cf649e09523de
Sadia Afreen Neela <69322639+Neela08@users.noreply.github.com>
2023-11-01 22:34:01 +0600

    reduced redundant steps

--- features/step-definitions/task.2.js
+++ features/step-definitions/task.2.js
@@ -3,7 +3,8 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 const HomePage = require('../pageobjects/home.page');
 const HoverPage = require('../pageobjects/hover.page');
 const AlertPage=require('../pageobjects/alert.page');
-const send_text='sadia'
+const keyPage = require('../pageobjects/key.page');
+let send_text=""
 const image_number=2;
 
 //test case 3
@@ -17,29 +18,38 @@ When(/^I click the hovers link$/,async() => {
 });
 
 Then(/^I am on the hovers page$/, async() => {
-	await expect(HoverPage.locate_User_Image(image_number)).toBeExisting()
+	await expect(HoverPage.locateUserImage(image_number)).toBeExisting()
    
 });
 
 When(/^I Hover over the profile image of user2$/, async() => {
     
-    await HoverPage.locate_User_Image(image_number).moveTo();
+    await HoverPage.locateUserImage(image_number).moveTo();
    
 });
 
 Then(/^I see name: user 2 and view profile link are displayed under the image on hover$/, async() => {
-    await expect(HoverPage.locate_User_Profile(image_number)).toBeDisplayed()
+    await expect(HoverPage.locateUserProfile(image_number)).toBeDisplayed()
 	
 });
+function generateRandomText(length) {
+   
+    let text = '';
 
+    for (let i = 0; i < length; i++) {
+      
+      const randomCharacter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+      text += randomCharacter;
+    }
+
+    return text;
+  }
 
 
 
 //test case 4
 
-Given(/^I am on the Home page$/, async() => {
-	await HomePage.open()
-});
 
 When(/^I click on the key press link$/, async() => {
 	
@@ -66,9 +76,6 @@ Then(/^I can see the message You entered: BACK_SPACE$/,async () => {
 //test case 5
 
 
-Given(/^I am on the Home page$/, async() => {
-	await HomePage.open()
-});
 
 When(/^I click on the JavaScript Alerts link$/, async() => {
     await HomePage.ClickJSAlertLink()
@@ -83,35 +90,42 @@ When(/^I press Click for JS Alert button$/, async() => {
 	await AlertPage.ClickJSAlert()
 });
 
-Then(/^I can see the Alert$/, async() => {
-    
-	let alertText = await browser.getAlertText() ;
-    expect(alertText).toEqual('I am a JS Alert');
+Then(/^User prompt with "([^"]*)" is shown$/, async(args1) => {
+	const alertText =  await browser.getAlertText() ;
+     expect(alertText).toEqual(args1);
 });
+
 
 When(/^I close the alert$/, async() => {
-	browser.acceptAlert()
+	await browser.acceptAlert()
 });
 
-Then(/^I can see the alert is closed$/, async() => {
-	const isAlertOpen = await browser.isAlertOpen();
-    expect(isAlertOpen).toBeFalsy();
+Then(/^I can see the user prompt is closed$/, async() => {
+	
+    
+    let flag=true
+    try{
+         flag=await browser.getAlertText()
+    }catch
+    {
+         flag=false
+    }
+    expect(flag).toBeFalsy();
 });
 
 When(/^I Click the Click for JS Confirm button$/, async() => {
 	await AlertPage.ClickJSConfirm()
 });
 
-Then(/^Confirm with "I am a JS Confirm" text is shown$/, async() => {
-    let alertText = await browser.getAlertText() ;
-    expect(alertText).toEqual('I am a JS Confirm');
+
+
+
+When(/^I click cancel button$/, async() => {
+	await browser.dismissAlert()
 });
 
-When(/^I Click the cancel button$/, async() => {
-	browser.dismissAlert()
-});
 
-Then(/^Confirm is closed and The message that you've clicked Cancel is shown under the results section$/, async() => {
+Then(/^The message that you've clicked Cancel is shown under the results section$/, async() => {
 	await expect(AlertPage.Result).toHaveTextContaining('You clicked: Cancel')
 });
 
@@ -121,23 +135,23 @@ When(/^I Click the Click for JS Prompt button$/, async() => {
 	await AlertPage.ClickJSPrompt()
 });
 
-Then(/^Prompt with "I am a JS Prompt" text is shown$/, async() => {
-	let alertText = await browser.getAlertText() ;
-    expect(alertText).toEqual('I am a JS prompt');
-});
+
 
 When(/^I Enter any text into the prompt and click ok$/, async() => {
-	browser.sendAlertText(send_text)
-    await browser.waitUntil(async () => await browser.isAlertOpen());
-	browser.acceptAlert()
+    send_text=generateRandomText(5);
+	await browser.sendAlertText(send_text)
+	await browser.acceptAlert()
 });
 
-Then(/^Prompt is closed. The text, that I have entered is displayed under the results section$/,async () => {
+Then(/^The text, that I have entered is displayed under the results section$/,async () => {
 
 
     await expect(AlertPage.Result).toHaveTextContaining(send_text);
 	
 });
+
+
+
 
 
 
