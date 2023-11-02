72b6e2c68d4fd339d6602ae0ea47c52ab78cbc98
Sadia Afreen Neela <69322639+Neela08@users.noreply.github.com>
2023-11-02 22:37:18 +0600

    update 4

---
+++ Utils/randomText.js
@@ -0,0 +1,18 @@
+const { $ } = require("@wdio/globals");
+class Randomizing {
+  generateRandomText(length) {
+    let text = "";
+
+    for (let i = 0; i < length; i++) {
+      const randomCharacter = String.fromCharCode(
+        Math.floor(Math.random() * 26) + 97
+      );
+
+      text += randomCharacter;
+    }
+
+    return text;
+  }
+}
+
+module.exports = new Randomizing();
