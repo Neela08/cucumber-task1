4a6fc6b55ad68eda540f64444080b52120ecd9f4
Sadia Afreen Neela <69322639+Neela08@users.noreply.github.com>
2023-11-02 19:54:08 +0600

    update 3

--- features/step-definitions/task.2.js
+++ features/step-definitions/task.2.js
@@ -1,143 +1,103 @@
-
-const { Given, When, Then } = require('@cucumber/cucumber');
-const HomePage = require('../pageobjects/home.page');
-const HoverPage = require('../pageobjects/hover.page');
-const AlertPage=require('../pageobjects/alert.page');
-const keyPage = require('../pageobjects/key.page');
-let send_text=""
-const image_number=2;
-const Randomizing=require('../../Utils/random.text') ;
-
+const { Given, When, Then } = require("@cucumber/cucumber");
+const HomePage = require("../pageobjects/home.page");
+const HoverPage = require("../pageobjects/hover.page");
+const AlertPage = require("../pageobjects/alert.page");
+const keyPage = require("../pageobjects/key.page");
+const imageNumber = 2;
+const Randomizing = require("../../Utils/random.text");
+const sendText=""
 //test case 3
 
-When(/^I click the hovers link$/,async() => {
-    await HomePage.ClickHoverLink()
+When(/^I click the hovers link$/, async () => {
+  await HomePage.ClickHoverLink();
 });
 
-Then(/^I am on the hovers page$/, async() => {
-	await expect(HoverPage.locateUserImage(image_number)).toBeExisting()
-   
+Then(/^I am on the hovers page$/, async () => {
+  await expect(HoverPage.isUserImageExisting(imageNumber));
 });
 
-When(/^I Hover over the profile image of user2$/, async() => {
-    
-    await HoverPage.locateUserImage(image_number).moveTo();
-   
+When(/^I Hover over the profile image of user2$/, async () => {
+  await HoverPage.locateUserImage(imageNumber).moveTo();
 });
 
-Then(/^I see name: user 2 and view profile link are displayed under the image on hover$/, async() => {
-    await expect(HoverPage.locateUserProfile(image_number)).toBeDisplayed()
-	
-});
-
-
-
+Then(
+  /^I see name: user 2 and view profile link are displayed under the image on hover$/,
+  async () => {
+    await expect(HoverPage.locateUserProfile(imageNumber)).toBeDisplayed();
+  }
+);
 
 //test case 4
 
-
-When(/^I click on the key press link$/, async() => {
-	
-    await HomePage.ClickKeyPageLink()
+When(/^I click on the key press link$/, async () => {
+  await HomePage.ClickKeyPageLink();
 });
 
-Then(/^I am on the Key Press page$/, async() => {
-	
-    await expect(keyPage.TextField).toBeExisting()
+Then(/^I am on the Key Press page$/, async () => {
+  await expect(keyPage.isTextFieldExisting());
 });
 
-When(/^I press Backspace key$/, async() => {
-    
-    await browser.keys(['Backspace'])
+When(/^I press Backspace key$/, async () => {
+  await browser.keys(["Backspace"]);
 });
 
-Then(/^I can see the message You entered: BACK_SPACE$/,async () => {
-	
-    await expect(keyPage.ResultField).toHaveTextContaining('You entered: BACK_SPACE')
+Then(/^I can see the message "([^"]*)"$/, async (args1) => {
+  await expect(keyPage.ResultField).toHaveTextContaining(args1);
 });
-
-
 
 //test case 5
 
-
-
-When(/^I click on the JavaScript Alerts link$/, async() => {
-    await HomePage.ClickJSAlertLink()
+When(/^I click on the JavaScript Alerts link$/, async () => {
+  await HomePage.ClickJSAlertLink();
 });
 
-Then(/^I am on the JavaScript Alerts page$/, async() => {
-	
-    await expect(AlertPage.JSAlert).toBeExisting()
+Then(/^I am on the JavaScript Alerts page$/, async () => {
+  await expect(AlertPage.isJSButtonExisting("Click for JS Alert"));
 });
 
-When(/^I press Click for JS Alert button$/, async() => {
-	await AlertPage.ClickJSAlert()
+When(/^I press "([^"]*)"$/, async (args1) => {
+  await AlertPage.clickJSButton(args1);
 });
 
-Then(/^User prompt with "([^"]*)" is shown$/, async(args1) => {
-	const alertText =  await browser.getAlertText() ;
-     expect(alertText).toEqual(args1);
+Then(/^User prompt with "([^"]*)" is shown$/, async (args1) => {
+  const alertText = await browser.getAlertText();
+  expect(alertText).toEqual(args1);
 });
 
-
-When(/^I close the alert$/, async() => {
-	await browser.acceptAlert()
+When(/^I close the alert$/, async () => {
+  await browser.acceptAlert();
 });
 
-Then(/^I can see the user prompt is closed$/, async() => {
-	
-    
-    let flag=true
-    try{
-         flag=await browser.getAlertText()
-    }catch
-    {
-         flag=false
-    }
-    expect(flag).toBeFalsy();
+Then(/^I can see the user prompt is closed$/, async () => {
+  let flag = true;
+  try {
+    flag = await browser.getAlertText();
+  } catch {
+    flag = false;
+  }
+  expect(flag).toBeFalsy();
 });
 
-When(/^I Click the Click for JS Confirm button$/, async() => {
-	await AlertPage.ClickJSConfirm()
+When(/^I click cancel button$/, async () => {
+  await browser.dismissAlert();
 });
 
+Then(
+  /^The message that you've clicked Cancel is shown under the results section$/,
+  async () => {
+    await expect(AlertPage.Result).toHaveTextContaining("You clicked: Cancel");
+  }
+);
 
-
-
-When(/^I click cancel button$/, async() => {
-	await browser.dismissAlert()
+When(/^I Enter any text into the prompt and click ok$/, async () => {
+  this.sendText = Randomizing.generateRandomText(5);
+  await browser.sendAlertText(sendText);
+  await browser.acceptAlert();
 });
 
-
-Then(/^The message that you've clicked Cancel is shown under the results section$/, async() => {
-	await expect(AlertPage.Result).toHaveTextContaining('You clicked: Cancel')
-});
-
-
-When(/^I Click the Click for JS Prompt button$/, async() => {
-	
-	await AlertPage.ClickJSPrompt()
-});
-
-
-
-When(/^I Enter any text into the prompt and click ok$/, async() => {
-    send_text =Randomizing.generateRandomText(5)
-	await browser.sendAlertText(send_text)
-	await browser.acceptAlert()
-});
-
-Then(/^The text, that I have entered is displayed under the results section$/,async () => {
-
-
-    await expect(AlertPage.Result).toHaveTextContaining(send_text);
-	
-});
-
-
-
-
-
-
-
+Then(
+  /^The text, that I have entered is displayed under the results section$/,
+  async () => {
+    await expect(AlertPage.Result).toHaveTextContaining(sendText);
+  }
+);
--- features/step-definitions/task.3.js
+++ features/step-definitions/task.3.js
@@ -1,38 +1,33 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-const { expect, $ } = require('@wdio/globals')
-const path=require('path')
-const DownloadPage = require('../pageobjects/download.page');
-const filename='text.txt';
-const fs=require('fs');
-const file_path=path.join(path.join(process.cwd(),"tempDownload"));
-
+const { Given, When, Then } = require("@wdio/cucumber-framework");
+const { expect, $ } = require("@wdio/globals");
+const path = require("path");
+const DownloadPage = require("../pageobjects/download.page");
+const filename = "text.txt";
+const fs = require("fs");
+const file_path = path.join(path.join(process.cwd(), "tempDownload"));
 
 async function assertFileIsDownloaded(filename) {
-    
-   
-    const downloadedFilePath = path.join(`${file_path}/${browser.capabilities.browserName}/${filename}`);
-    
-    const fileExists = await fs.existsSync(downloadedFilePath);
-  
-    expect(fileExists).toBe(true);
-  }
+  const downloadedFilePath = path.join(
+    `${file_path}/${browser.capabilities.browserName}/${filename}`
+  );
 
+  const fileExists = await fs.existsSync(downloadedFilePath);
 
-When(/^I click download files$/, async() => {
-	DownloadPage.open()
-    
+  expect(fileExists).toBe(true);
+}
+
+When(/^I click download files$/, async () => {
+  DownloadPage.open();
 });
 
-Then(/^I am on the download page$/,async () => {
-	await DownloadPage.downloadLink(filename).isDisplayed()
+Then(/^I am on the download page$/, async () => {
+  await DownloadPage.downloadLink(filename).isDisplayed();
 });
 
-When(/^I click on a file name$/,async () => {
-   
-	await DownloadPage.ClickDownloadLink(filename)
-    
+When(/^I click on a file name$/, async () => {
+  await DownloadPage.ClickDownloadLink(filename);
 });
 
-Then(/^i can see file is downloaded$/, async() => {
-	await assertFileIsDownloaded(filename);
+Then(/^i can see file is downloaded$/, async () => {
+  await assertFileIsDownloaded(filename);
 });
