341e23e7afabb269ff09d9a6cb2b5d314bc81141
Sadia Afreen Neela <69322639+Neela08@users.noreply.github.com>
2023-11-02 16:31:36 +0600

    task 3 for review

---
+++ features/pageobjects/download.page.js
@@ -0,0 +1,18 @@
+const Page = require('./page');
+
+
+class DownloadPage extends Page{
+     downloadLink(fileName) {
+      return $(`//*[@class="example"]//a[contains(text(),'${fileName}')]`);
+    
+      }
+async ClickDownloadLink(fileName)
+{
+return   this.downloadLink(fileName).click()
+}
+open () {
+    return super.open('download');
+}
+
+}
+module.exports = new DownloadPage();
